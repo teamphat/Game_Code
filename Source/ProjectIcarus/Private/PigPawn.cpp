3ed7a5b6bfd2a560f60a103396d8265d5ab95e3d
Duncan Budinger Jr <duncan.budinger@utah.edu>
2014-12-09 12:22:40 -0700

    ALL NEW ASSESTS AND CONTROLLERS

---
+++ Source/ProjectIcarus/Private/PigPawn.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectIcarus.h"
+#include "PigPawn.h"
+
+
+APigPawn::APigPawn(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/ProjectIcarus/Public/PigPawn.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "PigPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTICARUS_API APigPawn : public APawn
+{
+	GENERATED_UCLASS_BODY()	
+};
