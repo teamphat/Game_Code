21e9285de52dc0f09cc4d40ec3b6df318569cab0
rcanning <russ@acessinc.net>
2014-12-10 16:39:38 -0700

    Other assets before merge

--- Source/ProjectIcarus/ProjectIcarus.Build.cs
+++ Source/ProjectIcarus/ProjectIcarus.Build.cs
@@ -6,6 +6,6 @@ public class ProjectIcarus : ModuleRules
 {
 	public ProjectIcarus(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate" });
 	}
 }
